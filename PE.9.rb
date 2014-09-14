dadb5cd5b171eaee63e7ad1f4dfc45c39ffe715d
Mark Bennett <mwbennett@wesleyan.edu>
2014-09-14 12:06:32 -0400

    added solution to pe.9

--- PE.9.rb
+++ PE.9.rb
@@ -9,12 +9,14 @@ Find the product abc.
 
 =end 
 
-# If (a, b, c) is a pythagorean triplet, then so is (ka, kb, kc) for any k. Therefore, 
-# if we find a triplet such that a + b + c = 100, then the triplet (10a, 10b, 10c) is
-# our solution. 
-
 def triplet_product(target_sum) 
-  
-
+  (1..(target_sum / 2)).each do | m |
+    (1..m-1).each do | n |
+      a, b, c = [ m ** 2 - n ** 2, 2 * m * n, m ** 2 + n ** 2 ] 
+      return a * b * c if a + b + c == target_sum
+    end
+  end
 end 
+
+triplet_product(1000)
 
